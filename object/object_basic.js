beb1517b81dbbee1ecaf239f43f341f9cf493f73
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-04-20 17:39:54 +0200

    built create object

--- object/object_basic.js
+++ object/object_basic.js
@@ -2,12 +2,15 @@
 
 //create an object
 const dog={
-  "name": "Cat",
+  name: "Cat", // I call my dog cat to bother him
   "leg": 4,
   "tail": "short",
-  "enemies": ["cats", "shower"],
+  "enemies": ["cats", "shower"], // he doesn't like taking showers. That's one of the reason i call him cat
   "friends": ["dogfood", "bones"]
 };
+// between the properties, we use , but not ;
+// to affect a value to a key, we use : but not =
+// it's ok to not to put "" for keys (if there is no white spaces)
 
 
 

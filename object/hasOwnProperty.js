eb7e48ab70e5e0b3233da6530ac80aaa3077c0ff
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-04-20 17:29:34 +0200

    Update hasOwnProperty.js

--- object/hasOwnProperty.js
+++ object/hasOwnProperty.js
@@ -1,1 +1,10 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/testing-objects-for-properties
 
+// check wether the object "obj" has the property "checkProp". If it does, return that property's value. If not, return "Not found".
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp)){
+    return obj[checkProp]}
+  else{return "Not Found"}
+  // Only change code above this line
+}

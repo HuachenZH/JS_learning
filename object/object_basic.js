060933055f4a0ee158488f659d253f46a7bac84b
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-04-20 17:35:57 +0200

    Update object_basic.js

--- object/object_basic.js
+++ object/object_basic.js
@@ -1,5 +1,13 @@
 // basic operations of object
 
+//create an object
+const dog={
+  "name": "Cat",
+  "leg": 4,
+  "tail": "short",
+  "enemies": ["cats", "shower"],
+  "friends": ["dogfood", "bones"]
+};
 
 
 

9279b27025cc4669c0fea9276fcb74811bd6ea99
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-04-27 23:14:19 +0200

    Create remarque.js

---
+++ remarque.js
@@ -0,0 +1,7 @@
+// gs
+// https://www.youtube.com/watch?v=a4eSjE0yXRA&list=PL3rNcyAiDYK2_87aRvXEmAyD8M9DARVGK&index=6
+var name='Michael Jackson';
+gs.info(name);
+// this work with the instance of serviceNow, it will return: x_803884_test1: Michael Jackson
+// and this does not work with my local nodejs
+

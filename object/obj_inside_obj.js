a0c8d41073044e48bae60ecaec4194546e794d1f
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-04-27 21:37:42 +0200

    Create obj_inside_obj.js

---
+++ object/obj_inside_obj.js
@@ -0,0 +1,9 @@
+// is it possible to have an object inside an object?
+const obj1={
+	"first": "stay",
+	"obj2": {1:"clsr",2:"middle"}
+} // so yes it's possible
+
+// erros that i've made in this code:
+//1. I forgot the "const" when declaring obj1
+//2. I forgot the "," after line 3

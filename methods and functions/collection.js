14274161f8e83c1a547f69beb195456478564ee6
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-02 22:09:13 +0200

    .toString(), parseInt()

---
+++ methods and functions/collection.js
@@ -0,0 +1,12 @@
+// a collection of the methods that i've met
+// i dont think i can remember them all. Just come back to have a look when i have time
+
+// int to string, .toString()
+var tmp=65
+tmp=tmp.toString() // now tmp is '65'
+
+// string to int,   parseInt()
+// try parseInt
+var tmp='64'
+tmp=parseInt(tmp) // 64
+console.log(typeof tmp) // number

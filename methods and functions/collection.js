2812a8556e246089fc176654759aa1e55a988499
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-02 22:18:05 +0200

    Update collection.js

--- methods and functions/collection.js
+++ methods and functions/collection.js
@@ -2,11 +2,17 @@
 // i dont think i can remember them all. Just come back to have a look when i have time
 
 // int to string, .toString()
-var tmp=65
-tmp=tmp.toString() // now tmp is '65'
+var tmp=65;
+tmp=tmp.toString(); // now tmp is '65'
 
 // string to int,   parseInt()
 // try parseInt
-var tmp='64'
-tmp=parseInt(tmp) // 64
-console.log(typeof tmp) // number
+var tmp='64';
+tmp=parseInt(tmp); // 64
+console.log(typeof tmp); // number
+
+// string to float,  parseFloat()
+var tmp='66';
+var tmp_float=parseFloat(tmp);
+
+

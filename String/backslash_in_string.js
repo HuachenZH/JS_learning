4aeb3a01fe43a99a615132182cda0cb919769bed
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-02 21:31:42 +0200

    Update and rename quotes_in_string.js to backslash_in_string.js

---
+++ String/backslash_in_string.js
@@ -0,0 +1,18 @@
+
+//if we want quotes to be appear :
+var out='he said "i call my dog cat."'
+console.log(out) //he said " i call my dog cat."
+
+var out='c\'est pas facile'
+console.log(out) // c'est pas facile
+
+out='He said \"Voici comment on utilise les backslash:\n\\t\ttab\n\\n\tà la ligne\n\\"\t\"\n\\\'\t\'\n\\\t\\\n\"'
+/* output will be:
+He said "Voici comment on utilise les backslash:
+\t	tab
+\n	à la ligne
+\"	"
+\'	'
+\	\
+"
+*/

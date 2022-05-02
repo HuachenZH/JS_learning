611f04196fc9c79e92d5d4d369c1fab3975518fe
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-02 21:14:41 +0200

    Create quotes_in_string.js

---
+++ String/quotes_in_string.js
@@ -0,0 +1,7 @@
+
+//if we want quotes to be appear :
+var out='he said "i call my dog cat."'
+console.log(out) //he said " i call my dog cat."
+
+var out='c\'est pas facile'
+console.log(out) // c'est pas facile

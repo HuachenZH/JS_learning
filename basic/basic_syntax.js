a870c9c5b74abe033dfaa2024ea53dbc59d0b853
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-02 21:51:07 +0200

    Update basic_syntax.js

--- basic/basic_syntax.js
+++ basic/basic_syntax.js
@@ -1,3 +1,10 @@
 
 // typeof
+out=4;
+console.log('type of out is: ' + typeof out);
+out=out.toString();
+console.log(typeof out);
+console.log('type of out now becomes: ' + typeof out);
 
+// js is loosely typed language, we can have weird stuffs like:
+console.log('string and '+6+' ...number')

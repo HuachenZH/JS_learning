ceacdea229adf66f456093efdb2d27d5b5e60170
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-04 21:46:58 +0200

    equality and strict equality

--- basic/basic_syntax.js
+++ basic/basic_syntax.js
@@ -8,3 +8,11 @@ console.log('type of out now becomes: ' + typeof out);
 
 // js is loosely typed language, we can have weird stuffs like:
 console.log('string and '+6+' ...number')
+
+
+// equality and strict equality
+var a=1; // number
+var b='1'; // string
+console.log(a==b); // true. equality
+console.log(a===b); // false. strict equality
+

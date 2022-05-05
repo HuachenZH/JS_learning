1e3d1d92b5da211fb8a25e22c7538598e222ebe5
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-05 16:06:40 +0200

    scalar product with for

--- basic/basic_iteration_for.js
+++ basic/basic_iteration_for.js
@@ -5,3 +5,13 @@ arr=[];
 for(let i=0; i<5; i++) {
   arr.unshift(i); // as i used unshift, arr should be like 4 3 2 1 0
 }
+
+// a bit of practice : scalar product of two vector
+// solution with for
+  function dotProductVect(arr1,arr2) { // arr1 and arr2 must have the same length
+    var out=0;
+    for(var i=0; i<arr1.length; i++) {
+      out+=arr1[i]*arr2[i];
+    }
+    return out;
+  }

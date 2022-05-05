52417f4d3ff36ad2f4d7377a5226b9d048d6324c
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-05 11:22:07 +0200

    Create basic_iteration_while.js

---
+++ basic/basic_iteration_while.js
@@ -0,0 +1,22 @@
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/iterate-with-javascript-while-loops
+// Add the numbers 5 through 0 (inclusive) in descending order to myArray using a while loop.
+// expected [5, 4, 3, 2, 1, 0]
+
+// method 1 : i iterate descendently, push i into the array
+const myArray=[];
+let i=5;
+while(i>=0) {
+  myArray.push(i);
+  i--;
+}
+console.log(myArray);
+
+// method 2 : iiterate acsendently, unshift i into the arry
+const myArray=[];
+let i=0;
+while(i<=5) {
+  myArray.unshift(i);
+  i++;
+}
+console.log(myArray);
+

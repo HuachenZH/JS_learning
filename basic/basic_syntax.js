7458506a46b57bf3c32b64ea82d1df315258a9a7
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-07 09:20:49 +0200

    built ++i and i++

--- basic/basic_syntax.js
+++ basic/basic_syntax.js
@@ -16,3 +16,26 @@ var b='1'; // string
 console.log(a==b); // true. equality
 console.log(a===b); // false. strict equality
 
+// ++i and i++
+// ++i
+var i=0;
+var done=false;
+while(!done){
+  if(i<5){
+    console.log(++i + ' done='+done); // increment i first, then use i for console.log. The output will be 1 2 3 4 5
+    continue;
+  }
+  console.log('i think we are done');
+  done=true;
+}
+// i++
+var i=0;
+var done=false;
+while(!done){
+  if(i<5){
+    console.log(i++ + ' done='+done); // use i for console.log first, then increment i. The output will be 0 1 2 3 4
+    continue;
+  }
+  console.log('i think we are done');
+  done=true;
+}

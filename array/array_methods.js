4be420ea98aac05a57b87f29ad29312559e6d4a7
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-07 20:16:18 +0200

    built .join()

---
+++ array/array_methods.js
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=6WTHrE7lrhk&list=PL3rNcyAiDYK2_87aRvXEmAyD8M9DARVGK&index=31
+//.join() 
+// to concatenate the elements of an array to a string, and insert the argument of .join() between them.
+var list=['banana','grape','watermelon'];
+list1=list.join(', ');
+console.log(list);  // [ 'banana', 'grape', 'watermelon' ]
+console.log(typeof list); // object
+console.log(list1); // banana, grape, watermelon
+console.log(typeof list1);  // string

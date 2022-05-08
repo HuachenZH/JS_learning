ebf799c765f01194f7293458c946ab02a0bd0d62
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-08 18:56:06 +0200

    Update and rename gs_intro.js to gs_query.js

--- glideSystem/gs_query.js
+++ glideSystem/gs_query.js
@@ -20,3 +20,48 @@ while (incGr.next()) { // .next():  https://developer.servicenow.com/dev.do#!/re
   
   gs.info(incGr.getValue('number')); // .getValue():  https://developer.servicenow.com/dev.do#!/reference/api/sandiego/server/no-namespace/c_GlideRecordScopedAPI#r_ScopedGlideRecordGetValue_String
 } //                  ^ find the values of a field which is called 'number'. Then return the values as string
+
+
+
+//
+// L21S02 - Get and display numbers on all incidents v2
+//
+var incGr = new GlideRecord('incident');
+incGr.addQuery('active', true); 
+// https://developer.servicenow.com/dev.do#!/reference/api/sandiego/server/no-namespace/c_GlideRecordScopedAPI#r_ScopedGlideRecordAddQuery_String_Object
+// addQuery(String name, Object value)
+// Build a search query and return the rows that match the request. It's like  "where active=true"  in sql
+
+incGr.orderBy('number'); 
+// https://developer.servicenow.com/dev.do#!/reference/api/sandiego/server/no-namespace/c_GlideRecordScopedAPI#r_ScopedGlideRecordOrderBy_String
+// orderBy(String name)
+// Specifies an orderBy column. Results are in ascending order.
+
+incGr.setLimit(5);
+// https://developer.servicenow.com/dev.do#!/reference/api/sandiego/server/no-namespace/c_GlideRecordScopedAPI#r_ScopedGlideRecordSetLimit_Number
+// setLimit(Number maxNumRecords)
+// Sets the limit for number of records are fetched by the GlideRecord query.
+// we will only see 5 results 
+
+incGr.query(); // execute the query
+
+while (incGr.next()) {
+  gs.info(incGr.getValue('number'));
+}
+
+
+//
+// L21S03 - What is 'number’?
+//
+var incGr = new GlideRecord('incident');
+incGr.setLimit(1);
+incGr.query();
+
+if (incGr.next()) {
+  gs.info('dotNumber=' + typeof incGr.getValue('number') + ' gvNumber=' + typeof incGr.number);
+}
+// result:
+// x_803884_test1: dotNumber=string gvNumber=object
+// if we want a string, number... use .getValue()
+
+

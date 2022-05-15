da501f06aa1de6ef2e104d437c5f5d0edee5ea69
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-05-15 18:00:23 +0200

    built substring

--- String/string_methods.js
+++ String/string_methods.js
@@ -1,4 +1,4 @@
-// indexOf
+// indexOf  (get position (or index))
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 // The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
 // indexOf(searchElement)
@@ -17,3 +17,15 @@ world.forEach(element=>{console.log('We are looking for the position of: '+eleme
 // We are looking for the position of: abcxdz
 // -1      // when the world that we find does not exist, it returns -1
 
+
+
+// substring  (get a substring)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+// The substring() method returns the part of the string between the start and end indexes, or to the end of the string.
+// substring(indexStart)
+// substring(indexStart, indexEnd)
+var str = 'Approved: RITM0010001 - Laptop renewal';
+var pos = str.indexOf('RITM');
+console.log('RITM0010001'.length); // its length is 11
+var ritmNumber = str.substring(pos, pos + 11);
+console.log(ritmNumber); // it returns : RITM0010001

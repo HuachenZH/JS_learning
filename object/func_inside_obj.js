2c9448a18847f32065d173d46e69b44fc3fb58fa
Huachen ZHANG <huachen.zhang.23@eigsi.fr>
2022-06-03 11:06:48 +0200

    Create func_inside_obj.js

---
+++ object/func_inside_obj.js
@@ -0,0 +1,12 @@
+// Functions can also be property of an object
+var sister = {
+  name: "Sarah",
+  age: 23,
+  parents: [ "alice", "andy" ],
+  siblings: ["julia"],
+  favoriteColor: "purple",
+  pets: true,
+  paintPicture: function() { return "Sarah paints!"; }
+};
+
+sister.paintPicture();
